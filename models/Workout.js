2cc4837935a9fe3a9299dc17977b44f80c2825c6
Chris Gonzales <chrisgshred@gmail.com>
2020-08-03 17:47:05 -0700

    added schema, ran seed script w no errors

---
+++ models/Workout.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+
+    day: {
+        type: Date
+    },
+
+    exercises: {
+        type: []
+    },
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
